9f23d2503b54e4404a8aa942076a2d5a23ab0dff
Kamel R <kamel.r@wayne.edu>
2026-01-14 19:18:01 -0500

    Add project files.

---
+++ Student.cpp
@@ -0,0 +1,14 @@
+#include "Student.h"
+#include <iostream>
+using namespace std;
+void Student::printStudentInfo()
+{
+	cout << "Student Name:\n"<<name<<endl;
+	cout << "Student ID:\n"<<id<<endl;
+	cout << "Student Major\n"<<major<<endl;
+
+
+}
+
+
+
---
+++ Student.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "string"
+using namespace std; 
+
+class Student
+{
+private: 
+	string name; 
+	string id; 
+	string major;
+
+public: 
+	void printStudentInfo();
+
+};
+
